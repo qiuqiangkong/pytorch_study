5058afb500c001e8d69e9c8ac92d6d6b0a77bab9
qiuqiangkong <qiuqiangkong@gmail.com>
2018-09-10 15:23:49 +0100

    update

--- data_generator.py
+++ data_generator.py
@@ -1,45 +1,7 @@
 import numpy as np
 from utilities import load_data, calculate_scalar, scale
 
-
-# class DataGenerator(object):
-#     def __init__(self, batch_size, type, te_max_iter=None):
-#         assert type in ['train', 'test']
-#         self._batch_size_ = batch_size
-#         self._type_ = type
-#         self._te_max_iter_ = te_max_iter
-#         self.rs = np.random.RandomState(3492)
-#         
-#     def generate(self, xs, ys):
-#         x = xs[0]
-#         y = ys[0]
-#         batch_size = self._batch_size_
-#         n_samples = len(x)
-#         
-#         index = np.arange(n_samples)
-#         self.rs.shuffle(index)
-#         
-#         iter = 0
-#         epoch = 0
-#         pointer = 0
-#         while True:
-#             if (self._type_ == 'test') and (self._te_max_iter_ is not None):
-#                 if iter == self._te_max_iter_:
-#                     break
-#             iter += 1
-#             if pointer >= n_samples:
-#                 epoch += 1
-#                 if (self._type_) == 'test' and (epoch == 1):
-#                     break
-#                 pointer = 0
-#                 self.rs.shuffle(index)                
-#  
-#             batch_idx = index[pointer : min(pointer + batch_size, n_samples)]
-#             pointer += batch_size
-#             yield x[batch_idx], y[batch_idx]
-            
-            
-            
+  
 class DataGenerator(object):
 
     def __init__(self, batch_size, seed=1234):
